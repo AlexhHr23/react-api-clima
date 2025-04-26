51ed735b92b06224d407da00a44d93f00eb9d429
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-26 15:58:52 -0600

    Tipar con guards o assertions

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
-import { SearchType } from "../types";
+import { SearchType, Weather } from "../types";
+
+
+//Type Guard o assertions
+const isWeatherResponse = (weather : unknown) : weather is Weather => {
+    return (
+        Boolean(weather) &&
+        typeof weather === 'object' &&
+        typeof (weather as Weather).name === 'string' &&
+        typeof(weather as Weather).main.temp === 'number' &&
+        typeof(weather as Weather).main.temp_min === 'number' &&
+        typeof(weather as Weather).main.temp_max === 'number'
+    )
+}
 
 export const useWeather = () => {
 
@@ -9,7 +22,23 @@ export const useWeather = () => {
         const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
         const {data} = await axios(geoUrl);
-        console.log('data', data);
+
+        const lat = data[0].lat
+        const lon = data[0].lon
+
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+        //Castear type
+
+        // const {data: weatherResponse} = await axios<Weather>(weatherUrl)
+        // // console.log(weatherResponse.main.temp_max)
+
+        //Type guard
+        const {data: weatherResponse} = await axios(weatherUrl)
+        const result = isWeatherResponse(weatherResponse)
+        if(result) {
+            console.log(weatherResponse.name);
+        }
         
        }catch(error) {
         console.log(error);
